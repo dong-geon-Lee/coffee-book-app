f44ea5e09b2bcd52a5406898b07e6b0fc77e2ffa
이동건 <developer@idong-geon-ui-MacBookPro.local>
2022-12-18 10:33:04 +0900

    초기세팅 완료

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+    overflow-x: hidden;
+  }
+
+  body {
+    font-family: 'Inter', sans-serif;
+    /* font-family: 'Nanum Myeongjo', serif; */
+    /* font-family: 'Gothic A1', sans-serif; */
+    font-weight: 400;
+    line-height: 1.5;
+    overflow: hidden;
+    position: relative;
+  }
+`;
+
+export default GlobalStyle;
