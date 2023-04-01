d86b4aa735740038ac8f6203b2465d88caf61468
dong-geon-Lee <ch9716@naver.com>
2023-04-01 10:27:22 +0900

    Likes 컴포넌트 named export 적용(스타일,이미지)

--- src/pages/Likes/Likes.tsx
+++ src/pages/Likes/Likes.tsx
@@ -1,33 +1,12 @@
-import NavMenu from "../../components/NavMenu/NavMenu";
-import star from "../../assets/star.svg";
-import star0 from "../../assets/star3.svg";
-import back from "../../assets/back.svg";
-import heart from "../../assets/heart2.svg";
-import heart4 from "../../assets/heart4.svg";
-import coffee from "../../assets/coffee9.svg";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { accountListState, authUserState } from "../../recoils/userAuthState";
 import { productProps } from "../../@types/types";
 import { Link } from "react-router-dom";
 import { removeLikeItem } from "../../helpers/helpers";
 import { NOT__LIKE__COFFEE } from "../../constants/constants";
-import {
-  Background,
-  Container,
-  ContentBox,
-  Description,
-  EmptyBox,
-  Header,
-  Img,
-  ImgBox,
-  ItemBox,
-  Logo,
-  Section,
-  StarImg,
-  Stars,
-  Text,
-  Title,
-} from "./styles";
+import NavMenu from "../../components/NavMenu/NavMenu";
+import * as S from "./styles";
+import * as A from "../../assets";
 
 const Likes = () => {
   const authUser = useRecoilValue(authUserState);
@@ -50,51 +29,51 @@ const Likes = () => {
   };
 
   return (
-    <Container>
-      <Section>
-        <Header>
+    <S.Container>
+      <S.Section>
+        <S.Header>
           <Link to="/home">
-            <Logo src={back} alt="logo" />
+            <S.Logo src={A.back} alt="logo" />
           </Link>
-          <Title>좋아요</Title>
-          <Logo src={heart} alt="logo" className="logo" />
-        </Header>
+          <S.Title>좋아요</S.Title>
+          <S.Logo src={A.heart2} alt="logo" className="logo" />
+        </S.Header>
 
         {likeLists?.length >= 1 ? (
           likeLists.map((item: productProps) => (
-            <ItemBox key={item.id}>
-              <ImgBox>
-                <Img src={item.image} className="coffee__img" />
-                <Logo
-                  src={heart4}
+            <S.ItemBox key={item.id}>
+              <S.ImgBox>
+                <S.Img src={item.image} className="coffee__img" />
+                <S.Logo
+                  src={A.heart4}
                   className="heart"
                   onClick={() => handleLikes(Number(item.id))}
                 />
-              </ImgBox>
-              <ContentBox>
-                <Title>{item.title}</Title>
-                <Description>{item.description}</Description>
-                <Stars>
+              </S.ImgBox>
+              <S.ContentBox>
+                <S.Title>{item.title}</S.Title>
+                <S.Description>{item.description}</S.Description>
+                <S.Stars>
                   {item.stars?.map((stars, index) => (
-                    <StarImg
+                    <S.StarImg
                       key={index}
-                      src={stars === 1 ? star : star0}
+                      src={stars === 1 ? A.star : A.star0}
                       alt="star"
                     />
                   ))}
-                </Stars>
-              </ContentBox>
-            </ItemBox>
+                </S.Stars>
+              </S.ContentBox>
+            </S.ItemBox>
           ))
         ) : (
-          <EmptyBox>
-            <Background img={coffee}></Background>
-            <Text>{NOT__LIKE__COFFEE}</Text>
-          </EmptyBox>
+          <S.EmptyBox>
+            <S.Background img={A.coffee} />
+            <S.Text>{NOT__LIKE__COFFEE}</S.Text>
+          </S.EmptyBox>
         )}
-      </Section>
+      </S.Section>
       <NavMenu />
-    </Container>
+    </S.Container>
   );
 };
 
